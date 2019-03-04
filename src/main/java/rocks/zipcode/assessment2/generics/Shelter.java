39fdd459a10763764627a665e2ffa70bbdb52e12
James Coates <jamescoates@zipcoders-MacBook-Pro.local>
2019-03-04 14:42:20 -0500

    145passing

--- src/main/java/rocks/zipcode/assessment2/generics/Shelter.java
+++ src/main/java/rocks/zipcode/assessment2/generics/Shelter.java
@@ -1,6 +1,11 @@
 package rocks.zipcode.assessment2.generics;
 
 
+import rocks.zipcode.assessment2.generics.ageable.Ageable;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Shelter is a generic class that holds Objects that extends `Ageable`.
  * For example, if a Person and a Dog extends Ageable, then I can declare the following:
@@ -9,32 +14,35 @@ package rocks.zipcode.assessment2.generics;
  * Shelter<Dog> dogHouse = new Shelter<Dog>();
  */
 public class Shelter<_> {
+    List<Object> shelter;
     public Shelter() {
-        throw new NullPointerException();
+        this.shelter = new ArrayList<>();
     }
 
     /**
      * @return the number of item in the shelter
      */
     public int size() {
-        return -1;
+        return this.shelter.size();
     }
 
     public void add(Object object) {
+        this.shelter.add(object);
     }
 
     public Boolean contains(Object object) {
-        return null;
+        return this.shelter.contains(object);
     }
 
     public void remove(Object object) {
+        this.shelter.remove(object);
     }
 
     public Object get(Integer index) {
-        return null;
+        return this.shelter.get(index);
     }
 
     public Integer getIndexOf(Object ageable) {
-        return 0;
+        return this.shelter.indexOf(ageable);
     }
 }
--- src/main/java/rocks/zipcode/assessment2/objectorientation/Address.java
+++ src/main/java/rocks/zipcode/assessment2/objectorientation/Address.java
@@ -4,56 +4,91 @@ package rocks.zipcode.assessment2.objectorientation;
  * @author leon on 28/11/2018.
  */
 public class Address {
+    String line1;
+    String line2;
+    String city;
+    String state;
+    String zipcode;
+
     public Address() {
+        line1 = "";
+        line2 = "";
+        city = "";
+        state = "";
+        zipcode = "";
     }
 
     /**
      * @param addressLine1 - first address line
      * @param addressLine2 - second address line
-     * @param city - city of location
-     * @param state - state of city
-     * @param zipcode - zipcode of region
+     * @param city         - city of location
+     * @param state        - state of city
+     * @param zipcode      - zipcode of region
      */
     public Address(String addressLine1, String addressLine2, String city, String state, String zipcode) {
+        this.line1 = addressLine1;
+        this.line2 = addressLine2;
+        this.city = city;
+        this.state = state;
+        this.zipcode = zipcode;
     }
 
     public String getAddressLine1() {
-        return null;
+        return this.line1;
     }
 
     public void setAddressLine1(String addressLine1) {
+        this.line1 = addressLine1;
     }
 
     public String getAddressLine2() {
-        return null;
+        return this.line2;
     }
 
     public void setAddressLine2(String addressLine2) {
+        this.line2 = addressLine2;
     }
 
     public String getCity() {
-        return null;
+        return this.city;
     }
 
     public void setCity(String city) {
+        this.city = city;
     }
 
     public String getState() {
-        return null;
+        return this.state;
     }
 
     public void setState(String state) {
+        this.state = state;
     }
 
     public String getZipcode() {
-        return null;
+        return this.zipcode;
     }
 
     public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
     }
 
     @Override
     public boolean equals(Object o) {
-        return (Boolean)null;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Address address2 = (Address) o;
+        return line1 == address2.getAddressLine1() && line2 == address2.getAddressLine2() && city == address2.getCity() &&
+                state == address2.getState() && zipcode == address2.getZipcode();
     }
+
+    @Override
+    public String toString() {
+        return "Address{addressLine1='" + line1 + "', addressLine2='" + line2 + "', city='" + city + "', state='" + state + "', zipcode='" + zipcode + "'}";
+    }
+
 }
